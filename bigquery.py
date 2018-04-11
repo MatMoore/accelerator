84964c8821f94d52ab92a73257fcc525311dd021
Mat Moore <matmoore@users.noreply.github.com>
2018-04-11 17:49:08 +0100

    Use pandas_gbq

--- bigquery.py
+++ bigquery.py
@@ -3,10 +3,7 @@ from googleapiclient.discovery import build
 from oauth2client.service_account import ServiceAccountCredentials
 from google.oauth2 import service_account
 from os import environ
-
-SCOPES = ['https://www.googleapis.com/auth/bigquery'] # https://developers.google.com/identity/protocols/googlescopes#bigqueryv2]
-
-client = bigquery.Client.from_service_account_json('govuk_bigquery.json')
+import pandas_gbq
 
 # There are ~100,000 sessions involving search per day
 # and the estimated results size is around 1GB per week of data
@@ -30,15 +27,14 @@ CROSS JOIN UNNEST(hits.product) as product
 CROSS JOIN UNNEST(product.customDimensions) as customDimensions
 
 WHERE product.productListName = 'Site search results'
-AND _TABLE_SUFFIX BETWEEN '20180403' AND '20180403'
+AND _TABLE_SUFFIX BETWEEN '20180403' AND '20180410'
 AND product.productListPosition <= 20
 AND customDimensions.index = 71
 LIMIT 10
 '''
 
-# Bigquery client docs: https://googlecloudplatform.github.io/google-cloud-python/latest/bigquery/usage.html
-query_job = client.query(query)
-results = query_job.result()
+results = pandas_gbq.read_gbq(query, project_id='govuk-bigquery-analytics', private_key='govuk_bigquery.json', dialect='standard')
+results.to_csv('data/bigquery_results_20180403_20180410.csv', index=False)
 
 for row in results:
   print(row)
