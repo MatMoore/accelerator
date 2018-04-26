e32f7c60982201c5cd63a45c9526358111823b99
Mat Moore <matmoore@users.noreply.github.com>
2018-04-26 12:45:28 +0100

    Account for uncertainty in model parameters

--- estimate_relevance.py
+++ estimate_relevance.py
@@ -15,7 +15,7 @@ from checks import SeriesProperties
 
 logging.basicConfig(filename='estimate_relevance.log',level=logging.INFO)
 
-def ratio_error(num, den, num_err, den_err):
+def ratio_error(num, den, num_err, den_err, covariance=0):
     """
     Relative error of a ratio
 
@@ -23,11 +23,12 @@ def ratio_error(num, den, num_err, den_err):
     """
     num_term = num_err.divide(num).replace(np.inf, 0).fillna(0).pow(2)
     den_term = den_err.divide(den).replace(np.inf, 0).fillna(0).pow(2)
+    covariance_term = 2 * covariance.divide(num * den).replace(np.inf, 0).fillna(0)
 
-    variance = num_term + den_term
+    variance = num_term + den_term - covariance_term
     return variance.pow(1/2)
 
-def product_error(a, b, a_err, b_err):
+def product_error(a, b, a_err, b_err, covariance=0):
     """
     Relative error of a product of two values
 
@@ -35,15 +36,16 @@ def product_error(a, b, a_err, b_err):
     """
     a_term = a_err.divide(a).replace(np.inf, 0).fillna(0).pow(2)
     b_term = b_err.divide(b).replace(np.inf, 0).fillna(0).pow(2)
+    covariance_term = 2 * covariance.divide(a * b).replace(np.inf, 0).fillna(0)
 
-    variance = a_term + b_term
+    variance = a_term + b_term + covariance_term
     return variance.pow(1/2)
 
-def sum_error(a_err, b_err):
+def sum_error(a_err, b_err, covariance=0):
     """
     Absolute error of a sum of two values
     """
-    variance = a_err.pow(2) + b_err.pow(2)
+    variance = a_err.pow(2) + b_err.pow(2) + 2 * covariance
     return variance.pow(1/2)
 
 
